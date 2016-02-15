a3cf1c3c761e5c6f710b48708178ca7fe0536d91
henkensm <michael.henkens@ilias-solutions.com>
2016-02-15 13:34:49 +0100

    add code coverage

--- test/server.js
+++ test/server.js
@@ -20,6 +20,5 @@ describe('loading express', function(){
 
     it('404 everything else', function testPath(done){
         request(server).get('/foo/bar').expect(404, done);
-        
     });
 });
